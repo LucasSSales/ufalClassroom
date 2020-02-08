a709a21417ff512d1f8ef782daa5aebdcd6511ca
Lucas Sales <ldss@ic.ufal.br>
2020-02-08 01:50:06 -0300

    :rocket:creating more componets

---
+++ src/app/user-screen/registrate-class-dialog/registrate-class-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registrate-class-dialog',
+  templateUrl: './registrate-class-dialog.component.html',
+  styleUrls: ['./registrate-class-dialog.component.css']
+})
+export class RegistrateClassDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/user-screen/user-screen.component.ts
+++ src/app/user-screen/user-screen.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateClassDialogComponent } from './create-class-dialog/create-class-dialog.component';
+import { RegistrateClassDialogComponent } from './registrate-class-dialog/registrate-class-dialog.component';
 
 @Component({
   selector: 'app-user-screen',
@@ -8,17 +12,49 @@ import { Router } from '@angular/router';
 })
 export class UserScreenComponent implements OnInit {
 
-  materias = [{nome:"Linhas", prof:"Arturo"},{nome:"Etica", prof:"Olival"},{nome:"Gestão", prof:"Rafael Amorim"}]
-  loading = true;
+  materias = [
+    {nome:"Linhas", prof:"Arturo"},{nome:"Etica", prof:"Olival"},{nome:"Gestão", prof:"Rafael Amorim"},
+    {nome:"Linhas", prof:"Arturo"},{nome:"Etica", prof:"Olival"},{nome:"Gestão", prof:"Rafael Amorim"}
+  ]
+  loading = false;
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private api:ApiService, 
+    private route:ActivatedRoute, public dialog: MatDialog) { }
 
   ngOnInit() {
+    console.log(this.route.toString())
     //this.loading = false
+    this.api.getClassroom().subscribe(
+      data=>{
+        console.log(data)
+        this.loading = false;
+      },
+    );
   }
 
   openSubject(){
     this.router.navigateByUrl("/materia")
+  }
+
+  openDialog(type:string){
+    var dialogRef;
+    if(type=='create'){
+      dialogRef = this.dialog.open(CreateClassDialogComponent, {
+        width: '250px',
+        //data: {name: this.name, animal: this.animal}
+      });
+    }else if(type=='register'){
+      dialogRef = this.dialog.open(RegistrateClassDialogComponent, {
+        width: '250px',
+        //data: {name: this.name, animal: this.animal}
+      });
+    }
+
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+      //this.animal = result;
+    });
   }
 
 }
